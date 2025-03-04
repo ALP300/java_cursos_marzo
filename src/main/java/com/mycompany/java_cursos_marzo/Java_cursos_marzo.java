8baf88ca4f3e34f4bfcc512616c7c3482791081d
ALP300 <aitorpalomino1@gmail.com>
2025-03-03 19:44:15 -0500

    Subir proyecto

---
+++ src/main/java/com/mycompany/java_cursos_marzo/Java_cursos_marzo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_cursos_marzo;
+
+/**
+ *
+ * @author aitor
+ */
+public class Java_cursos_marzo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
